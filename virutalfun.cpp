828cc32fc8165bb4b30bf25ebe50e5f2f7d47c3c
srivithya24 <srivithyabose@gmail.com>
2026-03-03 12:11:04 +0530

    Add files via upload

---
+++ virutalfun.cpp
@@ -0,0 +1,81 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Person{
+protected:
+    string name;
+    int age;
+public:
+    Person(){
+        
+    }
+    virtual void getdata()=0;
+    virtual void putdata()=0;
+};
+class Professor : public Person{
+    int publications;
+    static int cur_id;
+public:
+    Professor(){
+        
+    }
+    void getdata(){
+        cin >> name >> age >>publications;
+    }
+    void putdata(){
+        cout << name <<" "<< age <<" "<<publications<<" "<<++cur_id<<endl;
+    }
+};
+int Professor::cur_id=0;
+class Student : public Person{
+    int marks[6];
+    int sum;
+    static int cur_id;
+    
+public:
+    Student(){
+        sum =0;
+    }
+    void getdata(){
+        cin >> name >> age ;
+        for(int i =0;i<6;i++){
+            cin >> marks[i];
+        }
+    }
+    void putdata(){
+        for(int i =0 ;i<6;i++){
+            sum += marks[i];
+        }
+        cout << name << " " << age << " "<<sum << " "<<++cur_id<<endl;
+    }
+};
+int Student::cur_id=0;
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
